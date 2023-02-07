67bbac74fd24713e64c33689de3e2dbcbb4f52f0
mc_jjn@163.com <mc_jjn@163.com>
2023-02-07 08:51:06 +0800

    23-2-7-0851 added TFloat class

---
+++ src/share/oop/TFloat.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Administrator on 2023-02-02.
+//
+
+#include "TFloat.hpp"
+
+TFloat::TFloat(EENDIAN _endian) {
+    this->endian = _endian;
+    _DEBUG_({
+        this->byte_value = 0;
+        this->c_value = 0;
+        this->fract = 0;
+        this->integer = 0;
+        this->_u_fract = 0;
+        this->_u_integer = 0;
+    });
+}
+double TFloat::get_cvalue() {
+    this->c_value = (double)this->integer + this->fract;
+    return this->c_value;
+}
+double TFloat::get_cvalue__FAST() {
+    return (double)((double)this->integer + ((double)this->fract));
+}
+void TFloat::set(u8 byte) {
+    this->byte_value = byte;
+    this->_u_integer = (u4)(byte >> /* 8 * 4 = */  32);
+    this->_u_fract   =     (byte & 0x00000000FFFFFFFF);
+//    EENDIAN_CODE(this->endian, { // not to wrap
+//        this->integer =                                          this->_u_integer;
+//        this->fract   = (double )(this->_u_fract)                  * 0.0000000001;
+//    }, { // must to wrap cuz _u_xx is in big endian
+//        this->integer =                         Endian::wrap_u4(this->_u_integer);
+//        this->fract   = (double )(Endian::wrap_u4(this->_u_fract)) * 0.0000000001;
+//    });
+// i dont know why but seems it doesnt need to wrap :(
+    this->integer = this->_u_integer;
+    this->fract   = (double )(this->_u_fract) * 0.0000000001;
+}
+void TFloat::set(double cv) {
+//    ERR(\n\n error: TFloat::set(double) is unsupported);
+    double __integer = 0;
+    this->fract      = modf(cv, &__integer);
+    this->integer    =       (int)__integer;
+    this->_u_integer =    (u4)this->integer;
+    this->_u_fract   = (u4)(this->fract * 10000000000);
+    this->byte_value = (
+            ((u8)this->_u_integer << 32) +
+            this->_u_fract
+    );
+    this->c_value = cv;
+}
---
+++ src/share/oop/TFloat.hpp
@@ -0,0 +1,31 @@
+//
+// Created by Administrator on 2023-02-02.
+//
+
+#ifndef $TVM_SRC_SHARE_TYPES_TFLOAT_HPP
+#define $TVM_SRC_SHARE_TYPES_TFLOAT_HPP
+
+#include "asm/BytesUtils.hpp"
+#include "utilities/macros.hpp"
+#include "asm/Endian.hpp"
+#include <math.h>
+
+class TFloat {
+public:
+    EENDIAN     endian;
+    u8     byte_value; // !this is bytes in big endian, not number!
+    u4     _u_integer; // !this is bytes in big endian, not number!
+    u4       _u_fract; // !this is bytes in big endian, not number!
+    double    c_value;
+    int       integer;
+    double      fract;
+
+    TFloat(EENDIAN _endian);
+    void              set(u8 byte); // create obj with u8 (in big endian)
+    void            set(double cv); // create with c value
+    double            get_cvalue(); // get double value in c
+    double FAST_FUNC(get_cvalue)(); // get double value in c without setting varible
+};
+
+
+#endif //$TVM_SRC_SHARE_TYPES_TFLOAT_HPP
