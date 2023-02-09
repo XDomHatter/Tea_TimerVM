231fd4e789867282e89d160533d25554e1915865
mc_jjn@163.com <mc_jjn@163.com>
2023-02-09 16:27:30 +0800

    23-2-9-1626 create class TimerVM

---
+++ src/share/run/TimerVM.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2023-02-09.
+//
+
+#ifndef $TVM_SRC_SHARE_RUN_TIMERVM_HPP
+#define $TVM_SRC_SHARE_RUN_TIMERVM_HPP
+
+
+class TimerVM {
+
+};
+
+
+#endif //$TVM_SRC_SHARE_RUN_TIMERVM_HPP
