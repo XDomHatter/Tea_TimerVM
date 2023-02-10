8f1613b350771b4d493dc86b8e4e625ce4adecb7
mc_jjn@163.com <mc_jjn@163.com>
2023-02-10 12:12:47 +0800

    23-2-10-1212 create file and add macro 'FILE_SKIP(size, file)'

---
+++ src/share/utilities/file_macros.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2023-02-09.
+//
+
+#ifndef $TVM_SRC_SHARE_UTILITIES_FILE_MACROS_HPP
+#define $TVM_SRC_SHARE_UTILITIES_FILE_MACROS_HPP
+
+#include <stdio.h>
+
+#define FILE_SKIP( size, file ) fgets(NULL, size, file);
+
+
+#endif //$TVM_SRC_SHARE_UTILITIES_FILE_MACROS_HPP
