2b6657f25e472fb24c8ce29dd8ad3f0d341fafa2
mc_jjn@163.com <mc_jjn@163.com>
2023-02-14 22:41:00 +0800

    23-2-14-2240 add readU1|2|4|8

--- src/share/codefile/TeaFileReader.cpp
+++ src/share/codefile/TeaFileReader.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "TeaFileReader.hpp"
+
+TeaFileReader::TeaFileReader(FILE *fileobj, EENDIAN eendian) {
+    this->file   = fileobj;
+    this->endian = eendian;
+}
+u1 TeaFileReader::readU1() {
+    return fgetc(this->file);
+}
+u2 TeaFileReader::readU2() {
+    u1 target[2] = {0};
+    target[0] = fgetc(this->file);
+    target[1] = fgetc(this->file);
+
+    return ((this->endian == ELITTlE_ENDIAN) ? ByteUtils::wrap_u2(*((u2 *)target)) // little endian
+                                             :                    *((u2 *)target));// big endian
+}
+u4 TeaFileReader::readU4() {
+    u1 target[4] = {0};
+    target[0] = fgetc(this->file);
+    target[1] = fgetc(this->file);
+    target[2] = fgetc(this->file);
+    target[3] = fgetc(this->file);
+    return ((this->endian == ELITTlE_ENDIAN) ? ByteUtils::wrap_u4(*((u4 *)target)) // little endian
+                                             :                    *((u4 *)target));// big endian
+}
+u8 TeaFileReader::readU8() {
+    u1 target[8] = {0};
+    target[0] = fgetc(this->file);
+    target[1] = fgetc(this->file);
+    target[2] = fgetc(this->file);
+    target[3] = fgetc(this->file);
+    target[4] = fgetc(this->file);
+    target[5] = fgetc(this->file);
+    target[6] = fgetc(this->file);
+    target[7] = fgetc(this->file);
+    return ((this->endian == ELITTlE_ENDIAN) ? ByteUtils::wrap_u8(*((u8 *)target)) // little endian
+                                             :                    *((u8 *)target));// big endian
+}
--- src/share/codefile/TeaFileReader.hpp
+++ src/share/codefile/TeaFileReader.hpp
@@ -5,9 +5,20 @@
 #ifndef $TVM_SRC_SHARE_CODEFILE_TEAFILEREADER_HPP
 #define $TVM_SRC_SHARE_CODEFILE_TEAFILEREADER_HPP
 
+#include <utilities/file_macros.hpp>
+#include <asm/BytesUtils.hpp>
+#include <asm/Endian.hpp>
 
 class TeaFileReader {
-
+private:
+    FILE * file;
+    EENDIAN endian;
+public:
+    TeaFileReader(FILE * fileobj, EENDIAN eendian);
+    u1 readU1();
+    u2 readU2();
+    u4 readU4();
+    u8 readU8();
 };
 
 
