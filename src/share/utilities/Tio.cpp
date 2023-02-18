f0fbd96a6594c426f0641eabd7726348a01e45a1
XDomHatter <mc_jjn@163.com>
2023-02-18 18:40:41 +0800

    23-2-18-1840 create file

---
+++ src/share/utilities/Tio.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2023-02-18.
+//
+
+#include "Tio.hpp"
---
+++ src/share/utilities/Tio.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2023-02-18.
+//
+
+#ifndef $TVM_SRC_SHARE_UTILITIES_TIO_HPP
+#define $TVM_SRC_SHARE_UTILITIES_TIO_HPP
+
+
+class TIO {
+
+};
+
+
+#endif //$TVM_SRC_SHARE_UTILITIES_TIO_HPP
