4fdb76fbe4e246d7a1a88519816f20971df6133a
XDomHatter <mc_jjn@163.com>
2023-02-18 19:05:12 +0800

    23-2-18-1903 initially complete CMDParser (#27)

--- src/share/run/CmdParser.cpp
+++ src/share/run/CmdParser.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "CmdParser.hpp"
+#include <utilities/cstr_utils.hpp>
+#include <utilities/Tio.hpp>
+
+CMDParser::CMDParser(int argc, char **argv) {
+    switch (argc) {
+        case 1:
+            // print help messeges
+            break;
+        case 2:
+            if(is_a_cmd(argv[1])){
+                switch (cast_cmd_type(argv[1])) {
+                    case HELPMSG:
+                        TIO::output("help message");
+                }
+
+            }
+    }
+}
+
+bool CMDParser::is_a_cmd(char *arg) {
+    return (arg[0] == '-');
+}
+enum CMD_TYPE CMDParser::cast_cmd_type(char *arg) {
+    if(cstr_EQUAL(arg, "-h")){
+        // help message
+        return HELPMSG;
+    }
+}
--- src/share/run/CmdParser.hpp
+++ src/share/run/CmdParser.hpp
@@ -5,8 +5,19 @@
 #ifndef $TVM_SRC_SHARE_RUN_CMDPARSER_HPP
 #define $TVM_SRC_SHARE_RUN_CMDPARSER_HPP
 
+enum CMD_TYPE{
+    HELPMSG, UNKNOWN
+};
 
 class CMDParser {
+public:
+    char * main_codefile;
+    char ** lib_names;
+    char ** lib_paths;
+    CMDParser(int argc, char ** argv);
+
+    static enum CMD_TYPE cast_cmd_type(char * arg);
+    static bool               is_a_cmd(char * arg);
 
 };
 
