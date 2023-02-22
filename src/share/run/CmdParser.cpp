609116007fda18510e12acd1e3e10833089100c5
XDomHatter <mc_jjn@163.com>
2023-02-22 20:04:03 +0800

    feature: add '-d' support and complete class
    now it supports '-v' '-d' '-h'
    
    Closes #30

--- src/share/run/CmdParser.cpp
+++ src/share/run/CmdParser.cpp
@@ -9,52 +9,93 @@
 #include <utilities/macros.hpp>
 
 CMDParser::CMDParser(int argc, char **argv) {
-    switch (argc) {
-        case 1:
-            // print help messeges
-            goto PRT_HPMSG;
-            break;
-        case 2:
-            if(is_a_cmd(argv[1])){
-                switch (cast_cmd_type(argv[1])) {
-                    case CMDT_HELPMSG:
-                    PRT_HPMSG:
-                        print_help();
-                        break;
-                    case CMDT_UNKNOWN:
-                        TIO::output("unknown command :");
-                        TIO::output(argv[1]);
-                        break;
-                    case CMDT_VERSION:
-                        TIO::output("Tea_TimerVM v1.0.0");
-                }
-            } else {
-                // try to find the file
-                FILE * file = fopen(argv[1], "r+");
-                if( file == NULL ) { // file doesn't exists
-                    TIO::output("cannot find file:");
-                    TIO::output(argv[1]);
-                    exit(0);
-                }
+    // init
+    this->recompile = false;
+    this->main_codefile = NULL;
+    this->lib_names     = NULL;
+    this->lib_paths     = NULL;
 
-            }
-    }
+    parse_cmd(argc, argv);
 }
 
+/// decide if 'arg' is a cmd
+/// @param arg a single arg
+/// @return if the arg is a command like '-h' '-v' etc.
 bool CMDParser::is_a_cmd(char *arg) {
     return (arg[0] == '-');
 }
-enum CMD_TYPE CMDParser::cast_cmd_type(char *arg) {
-    if(cstr_EQUAL(arg, "-h") || cstr_EQUAL(arg, "-H")){
+/// get type of arg ( now is -h -v -d )
+/// @param arg a single arg
+/// @return type of cmd
+enum CMD_TYPE CMDParser::decide_cmd_type(char *arg) {
+    if        (cstr_EQUAL(arg, "-h") || cstr_EQUAL(arg, "-H")) {
         // help message
         return CMDT_HELPMSG;
-    } else if(cstr_EQUAL(arg, "-v") || cstr_EQUAL(arg, "-V")){
+    } else if (cstr_EQUAL(arg, "-v") || cstr_EQUAL(arg, "-V")) {
         return CMDT_VERSION;
+    } else if (cstr_EQUAL(arg, "-d") || cstr_EQUAL(arg, "-D")) {
+        return CMDT_RECOMPILE;
     } else {
         return CMDT_UNKNOWN;
     }
 }
-
+/// parse cmd and do it
+/// @param argc count of arg
+/// @param argv value of arg
+void CMDParser::parse_cmd(int argc, char * argv[]) {
+    switch (argc) {
+        case 1:
+            print_help();
+            break;
+        case 2:
+            do_cmd(argv[1]);
+            break;
+        default:
+            // foreach in arguments array
+            for(int i = 0; i < argc; i++){
+                do_cmd(argv[i]);
+            }
+            break;
+    }
+}
+/// print help message
 void CMDParser::print_help() {
     TIO::output("help message");
 }
+/// do the match operation of single_arg
+/// @param single_arg the param to parse
+void CMDParser::do_cmd(char * single_arg) {
+    if(is_a_cmd(single_arg)){
+        switch (decide_cmd_type(single_arg)) {
+            case CMDT_HELPMSG:
+                print_help();
+                exit(0);
+                break;
+            case CMDT_RECOMPILE:
+                this->recompile = true;
+                break;
+            case CMDT_UNKNOWN:
+                TIO::output("unkown command: ");
+                TIO::output(single_arg);
+                exit(1);
+                break;
+            case CMDT_VERSION:
+                TIO::print_version();
+                exit(0);
+                break;
+        }
+    } else {
+        // try to find the file
+        FILE * file = fopen(single_arg, "r+");
+        if( file == NULL ) { // file doesn't exists
+            TIO::output("cannot find file:");
+            TIO::output(single_arg);
+            exit(0);
+        }
+        // file is correct, go on
+
+        this->main_codefile[this->codefile_count] = single_arg;
+        this->codefile_count ++;
+    }
+}
+
--- src/share/run/CmdParser.hpp
+++ src/share/run/CmdParser.hpp
@@ -6,19 +6,25 @@
 #define $TVM_SRC_SHARE_RUN_CMDPARSER_HPP
 
 enum CMD_TYPE{
-    CMDT_HELPMSG, CMDT_UNKNOWN, CMDT_VERSION
+    CMDT_HELPMSG, CMDT_UNKNOWN, CMDT_VERSION,
+    CMDT_RECOMPILE
 };
 
 class CMDParser {
 public:
-    char * main_codefile;
+    int codefile_count;
+    char ** main_codefile;
     char ** lib_names;
     char ** lib_paths;
+
+    bool recompile;
     CMDParser(int argc, char ** argv);
 
-    static enum CMD_TYPE cast_cmd_type(char * arg);
-    static bool               is_a_cmd(char * arg);
-    static void             print_help(          );
+    static enum CMD_TYPE decide_cmd_type(char * arg);
+    static bool          is_a_cmd       (char * arg);
+    static void          print_help     (          );
+           void          parse_cmd      (int argc, char * argv[]);
+           void          do_cmd         (char *single_arg);
 };
 
 
