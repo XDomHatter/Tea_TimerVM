60217986034a4e8ae7eb0ba7e4312f93d2a39a42
XDomHatter <mc_jjn@163.com>
2023-02-25 22:24:12 +0800

    feature: create class

---
+++ src/share/structures/BitMap.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2023-02-25.
+//
+
+#ifndef $TVM_SRC_SHARE_STRUCTURES_BITMAP_HPP
+#define $TVM_SRC_SHARE_STRUCTURES_BITMAP_HPP
+
+
+class BitMap {
+
+};
+
+
+#endif //$TVM_SRC_SHARE_STRUCTURES_BITMAP_HPP
