101d43759e5d90036165f20443407b8d17362ebe
XDomHatter <mc_jjn@163.com>
2023-02-25 15:30:57 +0800

    feature: create file

---
+++ src/share/structures/Utf8.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2023-02-25.
+//
+
+#include "Utf8.hpp"
---
+++ src/share/structures/Utf8.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2023-02-25.
+//
+
+#ifndef $TVM_SRC_SHARE_STRUCTURES_UTF8_HPP
+#define $TVM_SRC_SHARE_STRUCTURES_UTF8_HPP
+
+
+class UTF_8 {
+
+};
+
+
+#endif //$TVM_SRC_SHARE_STRUCTURES_UTF8_HPP
