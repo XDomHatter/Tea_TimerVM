8fafa43b0efc14227f62300ab7cd1f4dfdbacd93
XDomHatter <mc_jjn@163.com>
2023-02-25 23:22:29 +0800

    refactor: change 'Pointer.hpp' to 'Memory.hpp'

---
+++ src/share/asm/Memory.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Administrator on 2023-02-25.
+//
+
+#ifndef $TVM_SRC_SHARE_ASM_MEMORY_HPP
+#define $TVM_SRC_SHARE_ASM_MEMORY_HPP
+
+#include <asm/BytesUtils.hpp>
+#include <stdlib.h>
+
+typedef   u1 *     ptr;
+typedef void * address;
+
+address alloc_mem(int size){
+    return malloc(size);
+}
+
+#endif //$TVM_SRC_SHARE_ASM_MEMORY_HPP
