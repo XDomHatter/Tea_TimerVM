51ca361d0ce94e4b01188af47544144dbc07835a
XDomHatter <mc_jjn@163.com>
2023-03-03 18:21:30 +0800

    feature: add function 'error'
    add function 'error' for more detail error message

--- src/share/utilities/Tio.cpp
+++ src/share/utilities/Tio.cpp
@@ -10,14 +10,22 @@ void TConsole::input(char *buf) {
     buf = {0};
     gets((char *)buf);
 }
-void TConsole::output(char *str) {
+void TConsole::output(const char *str) {
     printf(str);
 }
 void TConsole::error(char *str) {
     output(str);
     exit(1);
 }
-
 void TConsole::print_version() {
     output("Tea_TimerVM v1.0.0 release");
+}
+void TConsole::error(const char *func, char *msg, char *paramerr) {
+    output(func);
+    output(": ");
+    output(msg);
+    output("\n");
+    output("param error: ");
+    output(paramerr);
+    exit(0);
 }
--- src/share/utilities/Tio.hpp
+++ src/share/utilities/Tio.hpp
@@ -8,9 +8,14 @@
 // only support english
 class TConsole {
 public:
-    static void output(char * str); // print a string(without new line) to stdout
+    static void output(const char * str); // print a string(without new line) to stdout
     static void input(char * buf);  // read a line from stdin
     static void error(char * str);
+    /// print a error message and quit
+    /// @param func the function where the error appear
+    /// @param msg error message
+    /// @param paramerr the error that the param created
+    static void error(const char *func, char * msg, char * paramerr);
     static void print_version();
 };
 
