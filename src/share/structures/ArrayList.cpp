29218ab687c8012470707840f2a6c9b7140ebfae
XDomHatter <mc_jjn@163.com>
2023-03-11 12:16:31 +0800

    feature: complete class
    complete 'get' 'set' 2 initializer and a destructor

--- src/share/structures/ArrayList.cpp
+++ src/share/structures/ArrayList.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "ArrayList.hpp"
+#include <stdlib.h>
+#include <string.h>
+#include <asm/Memory.hpp>
+
+ArrayList_str::ArrayList_str() {
+    this->list = (char **) Memory::alloc_mem((sizeof(char *)) * 16);
+    this->length = 16;
+    this->if_inited = new BitMap(16);
+}
+
+ArrayList_str::ArrayList_str(int length) {
+    this->list = (char **) Memory::alloc_mem((sizeof(char *)) * length);
+    this->length = length;
+    this->if_inited = new BitMap(length);
+}
+
+ArrayList_str::~ArrayList_str() {
+    for(int i = 0; i < this->length; i++){
+        if(this->if_inited->get(i)){
+            Memory::free_mem(*(this->list + (sizeof(char *) * i)));
+        }
+    }
+    Memory::free_mem(this->list);
+    delete this->if_inited;
+}
+
+char *ArrayList_str::get(int index) {
+    if (index >= this->length) {
+        return NULL;
+    }
+    if (this->if_inited->get(index) == 0) {
+        return NULL;
+    }
+    return this->list[index];
+}
+
+void ArrayList_str::set(int index, char *value) {
+    if (index > this->length) { // array need extend
+        /* extend list of strings*/
+        // usually we use this method to set value not only once
+        // so, for usability and also for machine can read that quickly,
+        // we do byte alignment:
+        // e.g. index = 13
+        // index % 4  = 3
+        // index + index % 4 = 13 + 3 = 16
+        int needed_size = index + (index % 4);
+        // add 8 byte if needed_size doesn't change( index is a multiples of 4 )
+        needed_size += ((index == needed_size) ? 8 : 0);
+
+        int old_length = this->length;
+        this->list = (char **) Memory::realloc_mem((pointer) this->list, needed_size * sizeof(char *));
+        this->length = needed_size;
+
+        /* extend bitmap */
+        this->if_inited->extend(needed_size - old_length);
+    }
+
+    if(this->if_inited->get(index) == 0){
+        // the string hasn't inited
+        // init c string(char *) by size of 'value'
+        // '+1' cuz 'strlen' doesn't contain '\0'
+        this->list[index] = (char *) Memory::alloc_mem(strlen(value) * sizeof(char) + 1);
+        this->if_inited->set(index, 1);
+    } else {
+        // it has inited so do reallocation
+        Memory::realloc_mem((pointer) this->list[index], strlen(value) * sizeof(char) + 1);
+    }
+
+    // this.list[index] = value;
+    strcpy(this->list[index], value);
+}
--- src/share/structures/ArrayList.hpp
+++ src/share/structures/ArrayList.hpp
@@ -5,8 +5,27 @@
 #ifndef $TVM_SRC_SHARE_STRUCTURES_ARRAYLIST_HPP
 #define $TVM_SRC_SHARE_STRUCTURES_ARRAYLIST_HPP
 
+#include <structures/BitMap.hpp>
 
-class ArrayList {
+class ArrayList_str {
+private:
+    char **list;
+    int length;
+    BitMap *if_inited;
+public:
+    ArrayList_str();
+    ArrayList_str(int length);
+    ~ArrayList_str();
+
+    /// get the str corresponding to the index
+    /// @param index Index of string
+    /// @return str of index
+    char *get(int index);
+
+    /// set the str corresponding to the index
+    /// @param index Index of string
+    /// @param value Value to set
+    void set(int index, char *value);
 
 };
 
