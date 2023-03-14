36e2bfcac6d9843e010677d5280732669a89e07d
XDomHatter <mc_jjn@163.com>
2023-03-14 20:10:54 +0800

    feature: finish 'lp' command
    use '-lp' to add library path
    use ArrayList_str replace char** to be more safe and easy
    
    Closes #34

--- src/share/run/CmdParser.cpp
+++ src/share/run/CmdParser.cpp
@@ -11,77 +11,88 @@
 CMDParser::CMDParser(int argc, char **argv) {
     // init
     this->recompile = false;
-    this->main_codefile = NULL;
-    this->lib_names     = NULL;
-    this->lib_paths     = NULL;
+    this->main_codefile = new ArrayList_str;
+    this->lib_paths = new ArrayList_str;
+    this->libpath_count = 0;
+    this->codefile_count = 0;
+    this->argv = argv;
 
     parse_cmd(argc, argv);
 }
 
-/// decide if 'arg' is a cmd
-/// @param arg a single arg
-/// @return if the arg is a command like '-h' '-v' etc.
 bool CMDParser::is_a_cmd(char *arg) {
     return (arg[0] == '-');
 }
-/// get type of arg ( now is -h -v -d )
-/// @param arg a single arg
-/// @return type of cmd
+
 enum CMD_TYPE CMDParser::decide_cmd_type(char *arg) {
     if        (cstr_EQUAL(arg, "-h") || cstr_EQUAL(arg, "-H")) {
-        // help message
         return CMDT_HELPMSG;
     } else if (cstr_EQUAL(arg, "-v") || cstr_EQUAL(arg, "-V")) {
         return CMDT_VERSION;
     } else if (cstr_EQUAL(arg, "-d") || cstr_EQUAL(arg, "-D")) {
         return CMDT_RECOMPILE;
+    } else if (cstr_EQUAL(arg, "-lp") || cstr_EQUAL(arg, "-LP")) {
+        return CMDT_LIBPATH;
     } else {
         return CMDT_UNKNOWN;
     }
 }
-/// parse cmd and do it
-/// @param argc count of arg
-/// @param argv value of arg
+
 void CMDParser::parse_cmd(int argc, char * argv[]) {
     switch (argc) {
         case 1:
             print_help();
             break;
         case 2:
-            do_cmd(argv[1]);
+            do_cmd(NULL);
             break;
         default:
             // foreach in arguments array
-            for(int i = 0; i < argc; i++){
-                do_cmd(argv[i]);
+            int i = 1;
+            for (; i < argc; i++) {
+                do_cmd(&i);
             }
             break;
     }
 }
-/// print help message
+
+
 void CMDParser::print_help() {
     TConsole::output("help message");
 }
-/// do the match operation of single_arg
-/// @param single_arg the param to parse
-void CMDParser::do_cmd(char * single_arg) {
-    if(is_a_cmd(single_arg)){
+
+
+void CMDParser::do_cmd(int *i) {
+    char *single_arg; // the arg to parse
+    single_arg = this->argv[
+            ((i == NULL) ? 1 : (*i))
+            // if i is NULL, use the first arg
+            // or use the the value of i
+    ];
+    if (is_a_cmd(single_arg)) {
         switch (decide_cmd_type(single_arg)) {
             case CMDT_HELPMSG:
                 print_help();
-                exit(0);
+                QUIT(0);
                 break;
             case CMDT_RECOMPILE:
                 this->recompile = true;
                 break;
-            case CMDT_UNKNOWN:
-                TConsole::output("unkown command: ");
-                TConsole::output(single_arg);
-                exit(1);
-                break;
             case CMDT_VERSION:
                 TConsole::print_version();
-                exit(0);
+                QUIT(0);
+                break;
+            case CMDT_LIBPATH:
+                // user call like: -lp "C:\\xxx\\"
+                if (i == NULL) TConsole::error("error: Not enough args");
+                (*i)++; // next arg ( after -lp )
+                this->lib_paths->set(this->libpath_count, argv[*i]);
+                this->libpath_count++;
+                break;
+            case CMDT_UNKNOWN:
+                TConsole::output("unknown command: ");
+                TConsole::output(single_arg);
+                exit(1);
                 break;
         }
     } else {
@@ -90,11 +101,13 @@ void CMDParser::do_cmd(char * single_arg) {
         if( file == NULL ) { // file doesn't exists
             TConsole::output("cannot find file:");
             TConsole::output(single_arg);
-            exit(0);
+            fclose(file);
+            exit(1);
         }
+        fclose(file);
         // file is correct, go on
 
-        this->main_codefile[this->codefile_count] = single_arg;
+        this->main_codefile->set(this->codefile_count, single_arg);
         this->codefile_count ++;
     }
 }
--- src/share/run/CmdParser.hpp
+++ src/share/run/CmdParser.hpp
@@ -5,28 +5,47 @@
 #ifndef $TVM_SRC_SHARE_RUN_CMDPARSER_HPP
 #define $TVM_SRC_SHARE_RUN_CMDPARSER_HPP
 
-enum CMD_TYPE{
+#include <structures/ArrayList.hpp>
+
+enum CMD_TYPE {
     CMDT_HELPMSG, // print help message
     CMDT_VERSION, // print version message
     CMDT_RECOMPILE, // recompile but not run
+    CMDT_LIBPATH, // add a library path for program
     CMDT_UNKNOWN, // unknown command
 };
 
 class CMDParser {
 public:
     int codefile_count;
-    char ** main_codefile;
-    char ** lib_names;
-    char ** lib_paths;
+    ArrayList_str * main_codefile;
+    int libpath_count;
+    ArrayList_str * lib_paths;
 
+    char **argv;
     bool recompile;
     CMDParser(int argc, char ** argv);
 
+    /// get type of arg ( now is -h -v -d )
+    /// @param arg a single arg
+    /// @return type of cmd
     static enum CMD_TYPE decide_cmd_type(char * arg);
+    /// decide if 'arg' is a cmd
+    /// @param arg a single arg
+    /// @return if the arg is a command like '-h' '-v' etc.
     static bool          is_a_cmd       (char * arg);
+    /// print help message
     static void          print_help     (          );
-           void          parse_cmd      (int argc, char * argv[]);
-           void          do_cmd         (char *single_arg);
+
+    /// parse cmd and do it
+    /// @param argc count of arg
+    /// @param argv value of arg
+    void parse_cmd(int argc, char *argv[]);
+
+    /// do the specific operation for an single arg
+    /// @param single_arg the param to parse
+    /// @param i          the current index of 'argv' ( usually in foreach )
+    void do_cmd(int *i);
 };
 
 
