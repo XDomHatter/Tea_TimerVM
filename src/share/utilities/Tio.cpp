8f10f5f4699d211ccc7a4bd3850f9d727d692cc5
XDomHatter <mc_jjn@163.com>
2023-03-16 20:20:04 +0800

    feature: change way to output and add codes for func 'output_m'
    1. print chars in console but not use 'printf'
    2. add function 'output_m' for print more strings in one line

--- src/share/utilities/Tio.cpp
+++ src/share/utilities/Tio.cpp
@@ -5,26 +5,48 @@
 #include "Tio.hpp"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <utilities/macros.hpp> // for QUIT()
 
 void TConsole::input(char *buf) {
     buf = {0};
     gets((char *) buf);
 }
 
-void TConsole::output(const char *str) {
-    printf(str);
+void TConsole::output(char *str) {
+    if(str == NULL) error("TConsole::output(const char *): str is MULL");
+    char * ptr = str;
+    while(*ptr != '\0') {
+        putchar(*ptr);
+        ptr++;
+    }
+}
+
+void TConsole::output_m(char *str, ...) {
+    output(str);
+    va_list ap;
+    va_start(ap, str);
+    char * sto; // string to output
+    for(;;) {
+        sto = va_arg(ap, char *);
+        if(sto == NULL) break;
+        output(sto);
+    }
+    va_end(ap);
+
+    return;
 }
 
 void TConsole::error(char *str) {
     output(str);
-    exit(1);
+    QUIT(1);
 }
 
 void TConsole::print_version() {
     output("Tea_TimerVM v1.0.0 release");
 }
 
-void TConsole::error(const char *func, char *msg, char *paramerr) {
+void TConsole::error(char *func, char *msg, char *paramerr) {
     output(func);
     output(": ");
     output(msg);
