b0db080b3ed7d658139c702b26bbbb4fe8882e59
XDomHatter <mc_jjn@163.com>
2023-03-18 13:53:10 +0800

    feature: delete some unuseful macros

--- src/share/utilities/macros.hpp
+++ src/share/utilities/macros.hpp
@@ -15,62 +15,10 @@
 #define STR(x)  #x
 #define XSTR(x) STR(x)
 
-// include from openjdk:
-// Apply pre-processor token pasting to the expansions of x and y.
-// The token pasting operator (##) prevents its arguments from being
-// expanded.  This macro allows expansion of its arguments before the
-// concatenation is performed.  Note: One auxilliary level ought to be
-// sufficient, but two are used because of bugs in some preprocesors.
-#define PASTE_TOKENS(x, y) PASTE_TOKENS_AUX(x, y)
-#define PASTE_TOKENS_AUX(x, y) PASTE_TOKENS_AUX2(x, y)
-#define PASTE_TOKENS_AUX2(x, y) x ## y
-
-#define CPU_HEADER_STEM(basename) PASTE_TOKENS(basename, INCLUDE_SUFFIX_CPU)
-#define OS_HEADER_STEM(basename) PASTE_TOKENS(basename, INCLUDE_SUFFIX_OS)
-
-// Include platform dependent files.
-//
-// This macro constructs from basename and INCLUDE_SUFFIX_OS /
-// INCLUDE_SUFFIX_CPU / INCLUDE_SUFFIX_COMPILER, which are set on
-// the command line, the name of platform dependent files to be included.
-// Example: INCLUDE_SUFFIX_OS=_linux / INCLUDE_SUFFIX_CPU=_sparc
-//   CPU_HEADER_INLINE(macroAssembler) --> macroAssembler_sparc.inline.hpp
-//   CPU_HEADER(macroAssembler) --> macroAssembler_sparc.hpp
-//
-// basename<cpu>.hpp / basename<cpu>.inline.hpp
-#define CPU_HEADER_H(basename)         XSTR(CPU_HEADER_STEM(basename).h)
-#define CPU_HEADER(basename)           XSTR(CPU_HEADER_STEM(basename).hpp)
-#define CPU_HEADER_INLINE(basename)    XSTR(CPU_HEADER_STEM(basename).inline.hpp)
-#define OS_HEADER_H(basename)         XSTR(OS_HEADER_STEM(basename).h)
-#define OS_HEADER(basename)           XSTR(OS_HEADER_STEM(basename).hpp)
-#define OS_HEADER_INLINE(basename)    XSTR(OS_HEADER_STEM(basename).inline.hpp)
-
-//x86
-#if defined(IA32) || defined(AMD64)
-#define X86
-#define X86_ONLY(code) code
-#define NOT_X86(code)
-#else
-#undef X86
-#define X86_ONLY(code)
-#define NOT_X86(code) code
-#endif
-
-#if defined(BIG_ENDIAN)
-#define ENDIAN_CODE(big, little) big
-#endif
-#ifdef  LITTLE_ENDIAN
-#define ENDIAN_CODE(big, little) little
-#endif
-
-
-#define CPU_HEADER_STEM(basename) PASTE_TOKENS(basename, INCLUDE_SUFFIX_CPU)
-#define OS_HEADER_STEM(basename) PASTE_TOKENS(basename, INCLUDE_SUFFIX_OS)
-
 #ifdef DEBUG
-#define _DEBUG_(code) code
+#define DEBUGCODE(code) code
 #else
-#define _DEBUG_(code)
+#define DEBUGCODE(code)
 #endif
 
 #define ERR(text)   \
