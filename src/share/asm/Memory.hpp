a3f4f2fcb5c1f1439bcf69fa7910f1e67fbd58a1
XDomHatter <mc_jjn@163.com>
2023-05-07 13:21:36 +0800

    feature: add helper macro Memory__alloc_mem_by_type
    save much redundant codes

--- src/share/asm/Memory.hpp
+++ src/share/asm/Memory.hpp
@@ -13,7 +13,12 @@ typedef void * address;
 
 class Memory {
 public:
+    /// same perform as malloc
     static pointer alloc_mem(size_t size);
+    /// use like: Memory__alloc_mem_by_type(2, u2)
+    /// it is same as Memory::alloc_mem(sizeof(u2) * 2)
+    #define Memory__alloc_mem_by_type(count, type)   \
+        Memory::alloc_mem(sizeof(type) * count)
     static pointer realloc_mem(pointer ptr, size_t size);
     static void free_mem(address mem);
     static void set_mem(pointer ptr, u1 value, size_t size);
