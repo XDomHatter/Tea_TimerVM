6c958d189c8c5e03341662885154afe57f8756da
XDomHatter <mc_jjn@163.com>
2023-07-03 11:19:26 +0800

    feature: package UTF-8 string and char
    1. UTF8Char packaged an utf-8 character(not only 1 byte).
    2. UTF8Char has some static method for utf8
    3. U8String packaged an utf-8 string.

--- src/share/structures/Utf8.cpp
+++ src/share/structures/Utf8.cpp
@@ -1,5 +1,188 @@
 //
 // Created by Administrator on 2023-02-25.
 //
-
 #include "Utf8.hpp"
+#include <asm/Memory.hpp>
+#include <utilities/cstr_utils.hpp>
+#include <utilities/Tio.hpp>
+
+
+///////////////////////////
+//////// UTF8Char /////////
+///////////////////////////
+UTF8Char::UTF8Char() {
+    this->c_str = "";
+    this->csize = 0;
+}
+UTF8Char::UTF8Char(u1 *bytes) {
+    int size = size_of_utf8Char(*bytes);
+    if(bytes[size - 1] == '\0') {
+        // that's a str not character
+        // just to copy characters ahead of '\0'
+        this->c_str = (char *) Memory::alloc_mem(size - 1);
+        Memory::copy(bytes, (pointer) this->c_str, size - 1);
+        this->csize = size - 1;
+    } else {
+        // A character without '\0'
+        this->c_str = (char *) Memory::alloc_mem(size);
+        Memory::copy(bytes, (pointer) this->c_str, size);
+        this->csize = size;
+    }
+}
+UTF8Char::UTF8Char(char ascii) {
+    this->csize = 1;
+    this->c_str = (char *) Memory::alloc_mem(1);
+    Memory::set_mem((pointer) this->c_str, ascii, 1);
+}
+UTF8Char::~UTF8Char() {
+    Memory::free_mem(this->c_str);
+}
+char *UTF8Char::get_cstr() {
+    return this->c_str;
+}
+
+int UTF8Char::cstr_size_of(UTF8Char *cs, int length) {
+    int size = 0;
+    for(int i = 0; i<length; i++){
+        size += cs[i].csize;
+    }
+    return size + 1; // +1 for '\0'
+}
+char *UTF8Char::cstr_of(UTF8Char *cs, int length) {
+    char *res = (char *) Memory::alloc_mem(cstr_size_of(cs, length));
+
+    // do the same as strcpy ( like strcpy(cs, res) )
+    UTF8Char * tmp_char;
+    int current_idx = 0;
+    for(int i = 0; i < length; i++) {
+        tmp_char = &cs[i];
+        Memory::copy((pointer) tmp_char->c_str,
+                     (pointer) (res+current_idx),
+                     tmp_char->csize);
+        current_idx += tmp_char->csize;
+        // e.g.
+        // i=0, cs->csize = 2
+        // looks like:
+        //      mov (c string of tmp char) -> (top addr of res)
+        //      current_idx += 2 (now is 2)
+        // i=1, cs->csize = 3
+        //      mov (c string of tmp char) -> (3rd (current_idx + 1) addr of res)
+        //      current_idx += 3 (now is 5)
+        // etc.
+    }
+
+    return res;
+}
+int UTF8Char::length_of_utf8String(char *cstr) {
+    int res = 0;
+    char c = *cstr;
+    for(int i = 0; c != '\0'; i++) {
+        i += size_of_utf8Char(c);
+        res ++;
+        c = cstr[i];
+    }
+    return res;
+}
+int UTF8Char::size_of_utf8Char(u1 c) {
+    if (c <= 0x7F) {                      // ASCII takes 1 bytes
+        return  1;
+    } else if (c >= 0xC0 && c <= 0xDF) {  // UTF-8 takes 2 bytes
+        return  2;
+    } else if (c >= 0xE0 && c <= 0xEF) {  // UTF-8 takes 3 bytes
+        return  3;
+    } else if (c >= 0xF0 && c <= 0xF7) {  // UTF-8 takes 4 bytes
+        return  4;
+    } else if (c >= 0xF8 && c <= 0xFB) {  // UTF-8 takes 5 bytes
+        return  5;
+    } else if (c >= 0xFC && c <= 0xFD) {  // UTF-8 takes 6 bytes
+        return  6;
+    } else if (c > 0x7F && c < 0xC0 ) {   // then is isn't first byte of utf-8
+        return -1;
+    } else {
+        return -2;
+    }
+}
+UTF8Char *UTF8Char::utf8Chars_of(char *cstr) {
+    int count = UTF8Char::length_of_utf8String(cstr);
+    UTF8Char *res = Memory::alloc_mem<UTF8Char>(count);
+    int idx_of_res = 0;
+    char * current_addr = cstr;
+    do {
+        res[idx_of_res] = UTF8Char((u1 *) current_addr);
+        idx_of_res ++;
+        current_addr = next_utf8Char(current_addr);
+    } while(idx_of_res < count);
+
+    return res;
+}
+char *UTF8Char::next_utf8Char(char *cstr) {
+    return cstr + size_of_utf8Char(*cstr);
+}
+
+
+/////////////////////////
+/////// U8String ////////
+/////////////////////////
+U8String::U8String() {
+    this->length = 0;
+    this->_so_chars = 0;
+    this->_chars = NULL;
+}
+U8String::U8String(const char* chars) {
+    this->_chars = (char *) malloc(
+            CSTRUtil::len((char *) (chars))
+            );
+//  this->_chars = chars;
+    CSTRUtil::copy(this->_chars, (char *) chars);
+    this->_so_chars = CSTRUtil::len((char *) chars);
+    this->length = UTF8Char::length_of_utf8String((char *) chars);
+}
+U8String::U8String(char *chars, int length) {
+    if (chars[length - 1] == '\0') { // str end with '\0'
+        this->_chars = (char *) Memory::alloc_mem(length);
+        //  this->_chars = chars;
+        CSTRUtil::copy(this->_chars, chars);
+        this->_so_chars = length;
+        this->length = UTF8Char::length_of_utf8String(chars);
+    } else {                         // string is without '\0'
+        this->_chars = (char *) Memory::alloc_mem(length + 1);
+        Memory::copy((pointer) chars, (pointer) this->_chars, length);
+        this->_chars[length] = 0;
+        this->_so_chars = length + 1; // +1 cuz '\0'
+        this->length = UTF8Char::length_of_utf8String(this->_chars);
+    }
+
+}
+U8String::U8String(UTF8Char *chars, int length) {
+    this->_so_chars = UTF8Char::cstr_size_of(chars, length);
+    this->_chars = UTF8Char::cstr_of(chars, length);
+    this->length = length;
+}
+bool U8String::isEmpty() {
+    return this->_chars == NULL  ||
+           cstr_EQUAL(this->_chars, "") || // this->_chars == ""
+           this->length    == 0  ||
+           this->_so_chars == 0;
+}
+U8String *U8String::join(U8String *str) {
+    this->length += str->length;
+    this->_so_chars += (str->_so_chars - 1); // -1 cuz _so_chars contains the '\0'
+
+    CSTRUtil::cat(&this->_chars, str->_chars);
+
+    return this;
+}
+char * U8String::get_utf8_char_at(int index) {
+    char *cur = this->_chars;
+    for(int i = 0; i < index; i++) {
+        cur += UTF8Char::size_of_utf8Char(*cur);
+    }
+    int cs = UTF8Char::size_of_utf8Char(*cur);
+    char *res = (char *) Memory::alloc_mem(cs);
+    Memory::copy((pointer) cur, (pointer) res, cs);
+    *(res+cs) = '\0'; // add '\0' to the end
+    return res;
+}
+char *U8String::get_cstr() {
+    return _chars;
+}
--- src/share/structures/Utf8.hpp
+++ src/share/structures/Utf8.hpp
@@ -5,9 +5,86 @@
 #ifndef $TVM_SRC_SHARE_STRUCTURES_UTF8_HPP
 #define $TVM_SRC_SHARE_STRUCTURES_UTF8_HPP
 
+#include <asm/BytesUtils.hpp>
 
-class UTF_8 {
+/// Store character in utf-8
+class UTF8Char {
+private:
+    char *c_str;
+public:
+    int csize; // count of byte
 
+    /// Create an empty character
+    UTF8Char();
+    /// Create utf-8 character from a existing utf-8(without '\0')
+    /// @param bytes Free it by caller! Method only copy it.
+    /// @param size Size of array(bytes)
+    explicit UTF8Char(u1 *bytes);
+    /// Create utf-8 from ascii char
+    explicit UTF8Char(char ascii);
+    ~UTF8Char();
+
+    char *get_cstr();
+
+    /// Calculate size for a c string to store utf8 chars
+    /// @param cs UTF8 Chars
+    /// @param length length of array(cs)
+    static int cstr_size_of(UTF8Char *cs, int length);
+
+    /// Change UTF8 chars into c string
+    /// @param cs UTF8 Chars
+    /// @param length length of array(cs)
+    /// @return Char array, remember to free it!
+    static char *cstr_of(UTF8Char *cs, int length);
+
+    /// Get count of character of a c string(one utf-8 takes 2 or more bytes)
+    /// @param cstr c string. (MUST END WITH '\0')
+    static int length_of_utf8String(char * cstr);
+
+    /// Calc size of an utf-8 character
+    /// @param c FIRST BYTE of utf-8 char
+    /// @return Size of bytes the utf-8 char takes
+    static int size_of_utf8Char(u1 c);
+
+    /// Box c string into a UTF8Char array
+    /// @param cstr c string(MUST END WITH '\0')
+    static UTF8Char * utf8Chars_of(char * cstr);
+
+    /// Move pointer to next character in a utf-8 string
+    /// !!! This method doesn't change the pointer !!!
+    /// @param cstr utf-8 string
+    /// @return pointer after moved(cstr)
+    static inline char * next_utf8Char(char *cstr);
+};
+
+class U8String {
+private:
+    int length; // count of utf-8 ( not that of byte )
+    char * _chars;
+
+public:
+    int _so_chars; // size of _chars
+
+
+    /// Create an empty string
+    U8String();
+    /// Init by utf8 characters
+    /// @param chars Only copy not refer!
+    /// @param length length of chars, not size
+    U8String(UTF8Char * chars, int length);
+    /// Init by c string constant
+    U8String(const char *chars);
+    /// Init by c string
+    U8String(char *chars, int length);
+
+    /// Concat a string at the end of self
+    U8String *join(U8String *str);
+    /// Get c string(char *) object
+    char * get_cstr();
+    /// Get character at index
+    char * get_utf8_char_at(int index);
+    /// Detect if string is empty
+    bool isEmpty();
 };
 
 
