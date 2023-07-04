1425ece3e4cb03d14de19e0fee142b0e9fc1d2cc
XDomHatter <mc_jjn@163.com>
2023-07-04 16:09:43 +0800

    feature: add `var` macro
    L warning:
     Clang-Tidy: Use auto when initializing with a cast to avoid duplicating the type name

--- src/share/utilities/macros.hpp
+++ src/share/utilities/macros.hpp
@@ -8,8 +8,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define var auto
 
-#define FAST_FUNC(func_name) func_name##__FAST
+#define FAST_FUNC(func_name) func_name##_fast
 
 //to change a token to string
 #define STR(x)  #x
