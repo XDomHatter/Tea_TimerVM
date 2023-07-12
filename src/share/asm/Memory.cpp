4fc096d7340577572d0a260ee6445e274d61335d
XDomHatter <mc_jjn@163.com>
2023-07-12 08:47:36 +0800

    feature: optimize function realloc_mem_keep_data
    Now it support reducing the pointer's size.

--- src/share/asm/Memory.cpp
+++ src/share/asm/Memory.cpp
@@ -13,7 +13,9 @@ pointer Memory::realloc_mem(pointer ptr, size_t size) {
 }
 pointer Memory::realloc_mem_keep_data(pointer *ptr, size_t size_of_old_ptr, size_t new_size) {
     pointer newptr = alloc_mem(new_size);
-    copy(*ptr, newptr, size_of_old_ptr);
+    copy(*ptr, newptr, (
+            (size_of_old_ptr >= new_size) ? new_size : size_of_old_ptr
+    ));
     free_mem(*ptr);
     *ptr = newptr;
     return newptr;
