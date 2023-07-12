cdde74dbdea3048fccf67177ad486e97e01db46b
XDomHatter <mc_jjn@163.com>
2023-07-12 16:12:23 +0800

    feature: add macro PATH_SEPARATOR
    To store different separator in different os.

--- src/share/utilities/file_macros.hpp
+++ src/share/utilities/file_macros.hpp
@@ -5,6 +5,15 @@
 #ifndef $TVM_SRC_SHARE_UTILITIES_FILE_MACROS_HPP
 #define $TVM_SRC_SHARE_UTILITIES_FILE_MACROS_HPP
 
+// path separator
+// In Windows, it is '\'
+// In Unix and Linux, it is '/'
+#if defined(OS_WINDOWS)
+#define PATH_SEPARATOR "\\"
+#else
+#define PATH_SEPARATOR "/"
+#endif
+
 #include <stdio.h>
 
 #define FILE_SKIP(size, file) fgets(NULL, size, file);
