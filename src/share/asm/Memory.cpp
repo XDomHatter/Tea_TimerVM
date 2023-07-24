b9b7ff6052848f77a7a3e3f17143a780708b5213
XDomHatter <mc_jjn@163.com>
2023-07-24 20:57:03 +0800

    perf: make some simple pack method inline

--- src/share/asm/Memory.cpp
+++ src/share/asm/Memory.cpp
@@ -5,12 +5,6 @@
 #include "Memory.hpp"
 #include <string.h>
 
-pointer Memory::alloc_mem(size_t size) {
-    return (pointer)malloc(size);
-}
-pointer Memory::realloc_mem(pointer ptr, size_t size) {
-    return (pointer)realloc(ptr, size);
-}
 pointer Memory::realloc_mem_keep_data(pointer *ptr, size_t size_of_old_ptr, size_t new_size) {
     pointer newptr = alloc_mem(new_size);
     copy(*ptr, newptr, (
@@ -19,12 +13,6 @@ pointer Memory::realloc_mem_keep_data(pointer *ptr, size_t size_of_old_ptr, size
     free_mem(*ptr);
     *ptr = newptr;
     return newptr;
-}
-void Memory::free_mem(address mem) {
-    free(mem);
-}
-void Memory::set_mem(pointer ptr, u1 value, size_t size) {
-    memset(ptr, value, size);
 }
 void Memory::copy(pointer source, pointer dest, size_t size) {
     for(int i = 0; i < size; i++) {
--- src/share/asm/Memory.hpp
+++ src/share/asm/Memory.hpp
@@ -6,6 +6,7 @@
 #define $TVM_SRC_SHARE_ASM_MEMORY_HPP
 
 #include <asm/BytesUtils.hpp>
+#include <string.h>
 #include <stdlib.h>
 
 typedef   u1 *     pointer;
@@ -14,15 +15,23 @@ typedef void * address;
 class Memory {
 public:
     /// same perform as malloc
-    static pointer alloc_mem(size_t size);
+    inline static pointer alloc_mem(size_t size) {
+        return (pointer) malloc(size);
+    }
     template<typename T>
     inline static T* alloc_mem(int count) {
         return (T*) alloc_mem(sizeof(T) * count);
     }
-    static pointer realloc_mem(pointer ptr, size_t size);
+    inline static pointer realloc_mem(pointer ptr, size_t size) {
+        return (pointer) realloc(ptr, size);
+    }
     static pointer realloc_mem_keep_data(pointer *ptr, size_t size_of_old_ptr, size_t new_size);
-    static void free_mem(address mem);
-    static void set_mem(pointer ptr, u1 value, size_t size);
+    inline static void free_mem(address mem) {
+        free(mem);
+    }
+    inline static void set_mem(pointer ptr, u1 value, size_t size) {
+        memset(ptr, value, size);
+    }
     /// move source -> dest
     static void copy(pointer source, pointer dest, size_t size);
 };
