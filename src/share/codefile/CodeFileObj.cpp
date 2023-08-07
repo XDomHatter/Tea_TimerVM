332f7419efed41f805d1d88bb9a23bf4db3c823a
XDomHatter <mc_jjn@163.com>
2023-08-07 12:48:45 +0800

    feat: create func `read_gf_info` `has_main_func` `read_global_funcs`
    - `read_gf_info` for reading global functions' info
    - `has_main_func` detect if there's main func in the file
    - `get_gf_fast` to get global function in file

--- src/share/codefile/CodeFileObj.cpp
+++ src/share/codefile/CodeFileObj.cpp
@@ -5,6 +5,7 @@
 
 #include "CodeFileObj.hpp"
 #include <codefile/CodeFileUtils.hpp>
+#include <algorithm>
 
 void CodeFileObj::read_pk(std::vector<char *> *lib_paths) {
     int count = parser->reader->nextU2_fast();
@@ -16,9 +17,12 @@ void CodeFileObj::read_pk(std::vector<char *> *lib_paths) {
     for(int j = 0; j < count; j++) {
         i = l[j];
         itCFO = new CodeFileObj();
-        FILE *f =CodeFileUtils::find_file(i, lib_paths);
 
-        if(f == NULL) TConsole::error(CSTRUtil::cat("cannot find file: ", i));
+        FILE *f =CodeFileUtils::find_file(i, lib_paths);
+        if(f == NULL) {
+            TConsole::output_f("cannot find file: %s", i);
+            QUIT(1);
+        }
         itCFO->parser = new TeaFileParser(
                 new TeaFileReader(f)
         );
@@ -33,6 +37,17 @@ void CodeFileObj::read_pk(std::vector<char *> *lib_paths) {
     set_status(STATUS_READEDPK);
 }
 
-void CodeFileObj::read_gv() {
-
+bool CodeFileObj::has_main_func() const {
+    return std::any_of(gf_infos->begin(), gf_infos->end(), [](METHOD_FUNCTION_Constant *i) -> bool {
+        return (
+            i->pkg_idx == 0                                                         &&
+            i->name->       equal(UTF8_Constant(5, (u1 *)"main")) &&
+            i->param_types->equal(UTF8_Constant(3, (u1 *)"[U"  )) &&
+            i->result_type->equal(UTF8_Constant(2, (u1 *)"4"   ))
+        );
+    });
+}
+void CodeFileObj::read_global_funcs() {
+    gf_map = parser->read_method_func(gf_infos, cp);
+    set_status(STATUS_READGF);
 }
--- src/share/codefile/CodeFileObj.hpp
+++ src/share/codefile/CodeFileObj.hpp
@@ -11,6 +11,7 @@
 #include <utilities/Tio.hpp>
 #include <utilities/cstr_utils.hpp>
 #include <vector>
+#include <list>
 
 struct CodeFileObj {
 private:
@@ -19,20 +20,26 @@ public:
     static const u1 VERIFY_MAGIC    = 1;
     static const u1 STATUS_READEDCP = 2;
     static const u1 STATUS_READEDPK = 4;
+    static const u1 STATUS_READGF   = 8;
 
-    char          *filename;
-    TeaFileParser *parser;
-    ConstantPool  *cp;
+
+    char                       *filename;
+    TeaFileParser              *parser;
+    ConstantPool               *cp;
     std::vector<CodeFileObj *> *packages;
-    TeaVariableSet *global_var;
+    TeaVariableSet             *global_var;
+    std::list<METHOD_FUNCTION_Constant *> *gf_infos;
+    std::map<METHOD_FUNCTION_Constant, TFunction *> *gf_map;
 
-    inline CodeFileObj(char *filename = NULL) {
+    inline explicit CodeFileObj(char *filename = NULL) {
         this->_status     = 0;
         this->parser      = NULL;
         this->cp          = NULL;
         this->filename    = filename;
         this->packages    = NULL;
         this->global_var  = NULL;
+        this->gf_infos    = NULL;
+        this->gf_map      = NULL;
     }
 
 
@@ -73,9 +80,23 @@ public:
         int count = parser->reader->nextU2();
         this->global_var = new TeaVariableSet(count);
     }
+    /// Read global functions info
+    inline void read_gf_info() {
+        int count = parser->reader->nextU2();
+        // every info is an u2, so guarantee count * 2 bytes
+        parser->reader->guarantee_more(count * 2);
+        this->gf_infos = parser->read_method_func_info(cp, count);
+    }
+    /// Determines whether there is a main function in the file
+    bool has_main_func() const;
+    /// read global function body
+    void read_global_funcs();
 
     inline CodeFileObj *get_pkg(int index) const {
         return this->packages->at(index);
+    }
+    inline TFunction *get_gf_fast(METHOD_FUNCTION_Constant index) const {
+        return this->gf_map->at(index);
     }
 };
 
