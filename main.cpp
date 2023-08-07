91f5abe426c41cebde4f952965dbc8711e6053d8
XDomHatter <mc_jjn@163.com>
2023-08-07 13:31:00 +0800

    chore: format code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <run/TimerVM.hpp>
 
-int main(int argc, char * argv[]){
-    var * vm = new TimerVM();
+int main(int argc, char *argv[]) {
+    var *vm = new TimerVM();
 
     CMDParser *cmd_parser = vm->parse_cmd(argc, argv);
     vm->open_files(cmd_parser);
-    vm->parse_files(cmd_parser);
+    vm->parse_files();
 
     delete vm;
     return 0;
