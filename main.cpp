9ff1c0cceffc499e7e5b4d6952b646d633b8ccfb
XDomHatter <mc_jjn@163.com>
2023-10-06 03:06:27 +0800

    feature: call vm api "exec_main_func"

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <run/TimerVM.hpp>
 
 int main(int argc, char *argv[]) {
+    TimerVM::initialize();
     var *vm = new TimerVM();
 
     CMDParser *cmd_parser = vm->parse_cmd(argc, argv);
     vm->open_files(cmd_parser);
     vm->parse_files();
+    vm->create_heap();
+    vm->exec_main_func(0, nullptr);
 
     delete vm;
     return 0;
